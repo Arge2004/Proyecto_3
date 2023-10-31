6f38f490e1e63b52efe2b37097a10c7c97aceac0
Arge2004 <argenis.medina@udea.edu.co>
2023-10-31 11:53:09 -0500

    Inicio el proyecto

---
+++ Proyecto3_Facturacion/src/main/java/com/mycompany/proyecto3_facturacion/Proyecto3_Facturacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto3_facturacion;
+
+/**
+ *
+ * @author argen
+ */
+public class Proyecto3_Facturacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
