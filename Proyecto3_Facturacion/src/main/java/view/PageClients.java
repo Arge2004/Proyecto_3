aa9fe8367d0a422f38fc7ca60966087ab90cac14
gotle <gotle@DESKTOP-D6B3UGO>
2023-11-04 16:33:09 -0500

    se agrego fotos y un nuevo jframe

--- Proyecto3_Facturacion/src/main/java/view/PageClients.java
+++ Proyecto3_Facturacion/src/main/java/view/PageClients.java
@@ -8,12 +8,12 @@ package view;
  *
  * @author gotle
  */
-public class PageBilling extends javax.swing.JFrame {
+public class PageClients extends javax.swing.JFrame {
 
     /**
      * Creates new form PageBilling
      */
-    public PageBilling() {
+    public PageClients() {
         initComponents();
     }
 
@@ -227,20 +227,21 @@ public class PageBilling extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PageBilling.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PageClients.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PageBilling.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PageClients.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PageBilling.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PageClients.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PageBilling.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PageClients.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new PageBilling().setVisible(true);
+                new PageClients().setVisible(true);
             }
         });
     }
