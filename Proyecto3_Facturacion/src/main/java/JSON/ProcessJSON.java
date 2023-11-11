2e083bf60a4233c1c8c7fdbb0566c3d07934d9f3
Arge2004 <argenis.medina@udea.edu.co>
2023-11-10 19:06:42 -0500

    JSON

--- Proyecto3_Facturacion/src/main/java/JSON/ProcessJSON.java
+++ Proyecto3_Facturacion/src/main/java/JSON/ProcessJSON.java
@@ -4,10 +4,34 @@
  */
 package JSON;
 
+import com.google.gson.Gson;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author argen
  */
 public class ProcessJSON {
-    
+    public class GrabarJson {
+
+     {
+        Gson gson = new Gson(); 
+        gson.toJson(p1);             
+        String cabeza = "Los pedidos del 9 de febrero";
+        String infomia1 = gson.toJson(p1);         
+        //Guardar
+            BufferedWriter bw = new BufferedWriter(new FileWriter()));
+            bw.write(cabeza);
+            bw.newLine();
+            bw.write(infomia1);
+            System.out.println("Archivo creado y primera persona registrada");
+        
+        // adicionar
+        p3.setNombre("JUAN");
+        p3.setApellidos("Ruiz");
+        p3.setEdad(60);
 }
